63dc689e8734f8a349f7c9a19ae3de4222afff62
damassofia <damassofia07@gmail.com>
2026-05-04 13:34:20 -0300

    Avance en las funciones de transaccion

--- ejercicio/ejercicio.c
+++ ejercicio/ejercicio.c
@@ -30,7 +30,7 @@ nodo_t *agregar_producto(nodo_t* lista, producto_t nuevo_producto)
         {
             nodo_t *actual = lista;
 
-            while (actual->siguiente != NULL && strcmp(actual->siguiente->datos.nombre_producto, nuevo_producto.nombre_producto) < 0)
+            while (actual->siguiente != NULL && strcmp(actual->siguiente->datos.codigo_producto, nuevo_producto.codigo_producto) < 0)
             {
                 actual = actual->siguiente;
             }
@@ -50,7 +50,7 @@ nodo_t *agregar_producto(nodo_t* lista, producto_t nuevo_producto)
 nodo_t *cargar_inventario(const char* nombre_archivo)
 {
     FILE* archivo = NULL;
-    Nodo* lista = NULL;
+    Nodo_t* lista = NULL;
 
     archivo = fopen(nombre_archivo , "r");
 
@@ -63,16 +63,16 @@ nodo_t *cargar_inventario(const char* nombre_archivo)
             //Al final de cada linea de texto agrega un terminador.
             linea[strcspn(linea , "\n")] = 0;
 
-            char* codigo_producto = strtok(NULL , ";");
+            char* codigo_producto = strtok(linea , ";");
             char* nombre_producto = strtok(NULL , ";");
             char* stock_texto = strtok(NULL , ";");
             char* punto_repo_texto = strtok(NULL , ";");
 
-            if(formato != NULL && codigo_producto != NULL && nombre_producto != NULL && stock_actual != NULL && punto_reposicion != NULL)
+            if(codigo_producto != NULL && nombre_producto != NULL && stock_texto != NULL && punto_repo_texto != NULL)
             {
                 producto_t nuevo_producto;
-                strcpy(nuevo_producto.codigo_producto , codigo_producto);
-                strcpy(nuevo_producto.nombre_producto , nombre_producto);
+                nuevo_producto.codigo_producto = strdup(codigo_producto);
+                nuevo_producto.nombre_producto = strdup(nombre_producto);
                 nuevo_producto.stock_actual = atoi(stock_texto);
                 nuevo_producto.punto_reposicion = atoi(punto_repo_texto);
 
@@ -89,3 +89,83 @@ nodo_t *cargar_inventario(const char* nombre_archivo)
     }
     return lista;
 }
+
+void procesar_transacciones(nodo_t** lista, const char* nombre_archivo)
+{
+    FILE* archivo = NULL;
+
+    archivo = fopen(nombre_archivo , "r");
+
+    if (archivo != NULL)
+    {
+        char linea[200];
+
+        while(fgets(linea, sizeof(linea) , archivo) != NULL)
+        {
+            linea[strcspn(linea , "\n")] = 0;
+            char* codigo_producto = strtok(linea , ";");
+            char* operacion = strtok(NULL , ";");
+            char* cantidad_texto = strtok(NULL , ";");
+
+            nodo_t *producto = buscar_producto(*lista , codigo_producto);
+
+        }
+        fclose(archivo);
+    }
+    else
+    {
+       //Funcion de mensajs de error
+    }
+}
+
+nodo_t *buscar_producto(nodo_t *lista, char* codigo_producto)
+{
+    nodo_t *actual = lista;
+    nodo_t *producto_encontrado = NULL;
+    int busqueda = 1;
+
+    while(actual != NULL && strcmp(actual->datos.codigo_producto, codigo_producto) <= 0)
+    {
+        busqueda = strcmp(actual->datos.codigo_producto , codigo_producto);
+        if(busqueda == 0)
+        {
+            producto_encontrado = actual;
+        }
+        actual = actual-> siguiente;
+    }
+    return producto_encontrado;
+}
+
+void transacciones(nodo_t **producto , char* operacion , int cantidad)
+{
+    if(*operacion == 'E')
+    {
+        entrada_stock(producto , cantidad);
+    }
+    else if(*operacion == 'S')
+    {
+        salida_stock(producto , cantidad);
+    }
+}
+
+void salida_stock(nodo_t *producto , int cantidad)
+{
+    if((producto->datos.stock_actual - cantidad) < 0)
+    {
+        //stderr
+    }
+    else if((producto->datos.stock_actual - cantidad) == 0)
+    {
+        //eliminar nodo
+    }
+    else
+    {
+        //nodifico el estado del nodo
+    }
+}
+
+void entrada_stock(nodo_t *producto , int cantidad)
+{
+    producto->datos.stock_actual = producto->datos.stock_actual + cantidad;
+}
+
