302b2b268b6167a75c1f363c23dd3981d8b7f790
damassofia <damassofia07@gmail.com>
2026-05-06 00:07:34 -0300

    Main !!! :)

--- ejercicio/main.c
+++ ejercicio/main.c
@@ -5,11 +5,40 @@
 #include <assert.h>
 #include <stdio.h>
 
-#include "libreria.h"
 #include "ejercicio.h"
 
 int main()
 {
 
+    nodo_t *inventario = NULL;
+
+    const char *archivo_maestro = "inventario_maestro.txt";
+    const char *archivo_transacciones = "transacciones.txt";
+    const char *archivo_nuevo_maestro = "nuevo_maestro.txt";
+    const char *archivo_urgentes = "compras_urgentes.txt";
+
+
+    printf("Cargando el inventario maestro...");
+    inventario = cargar_inventario(archivo_maestro);
+    printf("Inventario cargado!");
+
+    if (inventario == NULL)
+    {
+        fprintf(stderr, "Error crítico: No se pudo inicializar el inventario desde %s.\n", archivo_maestro);
+    }
+    else
+    {
+      printf("Procesando las transacciones ...\n");
+      procesar_transacciones(&inventario , archivo_transacciones);
+
+      escribir_maestro(inventario, archivo_nuevo_maestro);
+      printf("Se escrbió el nuevo inventario maestro!\n");
+
+      escribir_compras_urgentes(inventario, archivo_urgentes);
+      printf("Se creó el archivo de %s.\n" , archivo_urgentes);
+
+      liberar_lista(inventario);
+
+    }
     return 0;
 }
